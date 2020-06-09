b75f27ef8c8777c2f9c29f077e4d683bfd055382
Movshuk Vyacheslav <movshuk.vyacheslav@otr.ru>
2020-06-09 13:51:52 +0300

    to junit5

--- src/test/java/com/email/EmailApplicationTests.java
+++ src/test/java/com/email/EmailApplicationTests.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 
 @SpringBootTest
 class EmailApplicationTests {
@@ -19,7 +19,7 @@ class EmailApplicationTests {
     @Test
     void contextLoads() {
         logger.info("Run contextLoads test.");
-        Assert.assertNotEquals(controller, null);
+        Assertions.assertNotEquals(controller, null);
     }
 
 }
