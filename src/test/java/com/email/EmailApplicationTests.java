c62f1d384628d8de6b853a636223fad9f6242b42
Movshuk Vyacheslav <movshuk.vyacheslav@otr.ru>
2020-06-09 13:42:29 +0300

    create junit branch

--- src/test/java/com/email/EmailApplicationTests.java
+++ src/test/java/com/email/EmailApplicationTests.java
@@ -1,13 +1,25 @@
 package com.email;
 
+import com.email.controller.MessageController;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Assert;
 
 @SpringBootTest
 class EmailApplicationTests {
 
+    private Logger logger = LoggerFactory.getLogger(EmailApplicationTests.class);
+
+    @Autowired
+    private MessageController controller;
+
     @Test
     void contextLoads() {
+        logger.info("Run contextLoads test.");
+        Assert.assertNotEquals(controller, null);
     }
 
 }
